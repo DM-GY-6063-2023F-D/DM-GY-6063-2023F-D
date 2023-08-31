be8cdbf37ff717b80cc4c373b9cd2dfd1df98f00
Thiago Hersan <thiago.hersan+github@gmail.com>
2023-08-30 22:58:16 -0400

    fix drop-down colors and adds arrow

--- js/homework.js
+++ js/homework.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const act = document.getElementsByClassName("assignment-content-title");
   Array.from(act).forEach((a) => {
-    a.addEventListener("click", (ev) => {
-      ev.target.classList.toggle("show");
-      ev.target.nextElementSibling.classList.toggle("show");
+    a.addEventListener("click", (_) => {
+      a.classList.toggle("show");
+      a.nextElementSibling.classList.toggle("show");
     });
   });
 });
--- js/schedule.js
+++ js/schedule.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const wt = document.getElementsByClassName("week-title");
   Array.from(wt).forEach((a) => {
-    a.addEventListener("click", (ev) => {
-      ev.target.classList.toggle("show");
-      ev.target.nextElementSibling.classList.toggle("show");
+    a.addEventListener("click", (_) => {
+      a.classList.toggle("show");
+      a.nextElementSibling.classList.toggle("show");
     });
   });
 });
